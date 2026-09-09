60699dacb5da8e7a68f429405e1c7f990a06c02b
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-09 14:14:28 +0530

    Ignore Java compiled files

---
+++ array-2-pointer/reverseArray.java
@@ -0,0 +1,22 @@
+public class reverseArray{
+    public static void Reversearray(int arr[]){
+        int left = 0; 
+        int right = arr.length -1;
+        while(left < right){
+           int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }  
+         
+    }
+
+   public static void main(String[] args) {
+    int arr[] = {1,2,3,4,5};
+  Reversearray(arr);
+  for(int i = 0; i < arr.length; i++){
+    System.out.print(arr[i] + " ");
+   }
+}
+}
