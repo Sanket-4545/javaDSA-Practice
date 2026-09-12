c3cdbe2beacd2128949f54ad381881259d55871b
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-12 09:54:58 +0530

    closest Sum

---
+++ array-2-pointer/closestSum.java
@@ -0,0 +1,40 @@
+public class closestSum {
+
+    public static void closestSum(int arr[], int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        int closestSum = Integer.MAX_VALUE;
+        int first = 0;
+        int second = 0;
+
+        while (left < right) {
+
+            int sum = arr[left] + arr[right];
+
+            if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                closestSum = sum;
+                first = arr[left];
+                second = arr[right];
+            }
+
+            if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                break;
+            }
+        }
+
+        System.out.print(first + " " + second);
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 2, 4, 5, 7, 10};
+
+        closestSum(arr, 8);
+    }
+}
