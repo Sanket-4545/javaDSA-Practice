f467d09b31ef63cdba7a549c9f94be9c3fc16ea8
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-25 14:48:05 +0530

    prefix sum basic

---
+++ Prefix Sum/PrefixSumBasic.java
@@ -0,0 +1,15 @@
+public class PrefixSumBasic {
+public static void main(String[] args) {
+ int arr[] = {2, 4, 3, 5, 1};
+int prefix[] = new int[arr.length];
+prefix[0] = arr[0];
+for(int i = 1 ; i< arr.length;i++){
+    prefix[i] = prefix[i - 1] + arr[i];
+    
+}  
+for(int i = 0 ;i <prefix.length;i++){
+    System.out.print(prefix[i] + " ");
+} 
+
+}    
+}
