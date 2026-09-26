d26590559af635602f863398c15b4ae09c5cdfe1
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-26 21:25:16 +0530

    fix the error

--- 3.Prefix Sum/rangeSumUsingPrefixSum.java
+++ 3.Prefix Sum/rangeSumUsingPrefixSum.java
@@ -1,31 +1,67 @@
 
+
 public class rangeSumUsingPrefixSum {
-    public static int[] prefixSum(int arr[]) {
-        int prefix[] = new int[arr.length];
+
+    // Method to create Prefix Sum array
+    public static int[] prefixSum(int[] arr) {
+
+        int[] prefix = new int[arr.length];
+
         prefix[0] = arr[0];
+
         for (int i = 1; i < arr.length; i++) {
             prefix[i] = prefix[i - 1] + arr[i];
         }
+
         return prefix;
     }
 
-    public static int RangeSumUsingPrefixSum(int[] arr, int left, int right) {
-    
-    int prefix[] = prefixSum(arr);
-    return prefix[right] - prefix[left - 1];
-}
-    public static void main(String[] args) {
-        int arr[] = { 3, 5, 2, 7, 4 };
-        int sum[] = prefixSum(arr);
+    // Method to find range sum using Prefix Sum
+    public static int rangeSumUsingPrefixSum(int[] arr, int left, int right) {
 
-        for (int i = 0; i < sum.length; i++) {
-            System.out.print(sum[i] +" ");
+        int[] prefix = prefixSum(arr);
+
+        if (left == 0) {
+            return prefix[right];
         }
-       
-        int left = 1;   
-        int right = 3;
-        System.out.println();
-        System.out.println("Range Sum from index " + left + " to " + right + ": " + RangeSumUsingPrefixSum(arr, left, right));  
+
+        return prefix[right] - prefix[left - 1];
     }
 
+    public static void main(String[] args) {
+
+        int[] arr = {3, 5, 2, 7, 4};
+
+        // Create Prefix Sum
+        int[] prefix = prefixSum(arr);
+
+        System.out.print("Prefix Sum: ");
+
+        for (int i = 0; i < prefix.length; i++) {
+            System.out.print(prefix[i] + " ");
+        }
+
+        System.out.println();
+
+        // Range 1 to 3
+        int left = 1;
+        int right = 3;
+
+        int result = rangeSumUsingPrefixSum(arr, left, right);
+
+        System.out.println(
+            "Range Sum from index " + left + " to " + right + ": " + result
+        );
+
+        // Range 0 to 3
+        left = 0;
+        right = 3;
+
+        result = rangeSumUsingPrefixSum(arr, left, right);
+
+        System.out.println(
+            "Range Sum from index " + left + " to " + right + ": " + result
+        );
+    }
 }
+
